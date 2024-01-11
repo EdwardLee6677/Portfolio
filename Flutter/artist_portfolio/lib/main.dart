0364b80bda0e5137832a3583a294c03e9e76a444
EdwardLee <aandd67@gmail.com>
2024-01-11 18:31:38 +0900

    코드 수정 후

--- Flutter/artist_portfolio/lib/main.dart
+++ Flutter/artist_portfolio/lib/main.dart
@@ -10,7 +10,42 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Image.asset('assets/images/dragonlordgrave.png'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Artist Portfolio'),
+          backgroundColor: Colors.green,
+        ),
+        body: Column(
+          children: [
+            Container(
+              width: 200, 
+              height: 200, 
+              color: Colors.green,
+              margin: EdgeInsets.all(00),
+            ),
+            Container(
+              width: 200, 
+              height: 200, 
+              color: Colors.yellow,
+              margin: EdgeInsets.all(20),
+            ),
+            Container(
+              width: 200, 
+              height: 200, 
+              color: Colors.red,
+              padding: EdgeInsets.all(20),
+              child: Text('Hello World!'),
+            ),
+            Container(
+              width: 200, 
+              height: 200, 
+              color: Colors.blue,
+              padding: EdgeInsets.fromLTRB(10, 40, 20, 20),
+              child: Text('Hello World!'),
+            ),
+          ],
+        )  
+      )
     );
   }
 }
