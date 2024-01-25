850db13b60c14fe4dfda0eedbe1bea2faa8c8099
EdwardLee <aandd67@gmail.com>
2024-01-25 18:43:48 +0900

    앱 개발 시작

--- Flutter/Project/artist/lib/main.dart
+++ Flutter/Project/artist/lib/main.dart
@@ -1,18 +1,48 @@
 import 'package:flutter/material.dart';
 
+
 void main() {
-  runApp(const MainApp());
+  runApp(MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        appBar: AppBar(
+          title: Text('앱 타이틀'),
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.search),
+              onPressed: () {
+                // 검색 기능 구현
+              },
+            ),
+          ],
+        ),
+        body: ListView(
+          children: <Widget>[
+            Container(
+              height: 200,
+              color: Colors.grey, // 배너 자리
+              child: Center(child: Text('banner')),
+            ),
+            // 여기에 다른 콘텐츠 영역을 구현
+          ],
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.shuffle),
+              label: 'Random',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.notifications),
+              label: 'Notifications',
+            ),
+          ],
+          // 여기에 탭 기능을 추가할 수 있음
         ),
       ),
     );
